1c00c423217c1bb7a2efcec825f0d04cdc92af1c
Alex Mathews <alexjmathews@gmail.com>
2017-05-14 18:37:48 -0400

    Working lock file, initial eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "indent": ["error", "tab"],
+        "no-tabs": ["off"],
+        "react/jsx-indent": ["error", "tab"],
+        "comma-dangle": ["off"]
+    }
+};
---
+++ app/components/Main.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const Main = React.createClass({
+	render() {
+		return (
+			<div>
+				Hello World
+			</div>
+		);
+	},
+});
