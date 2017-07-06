6a79b0def6aad103801a4ea05e025ea2f7b5cfd5
Nikita Singh <nktasingh@gmail.com>
2017-07-06 14:59:37 -0400

    Patient bundle

--- app/routes/Home.jsx
+++ app/routes/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import PatientContainer from '../modules/patients';
+// import PatientContainer from '../modules/patients';
+import PatientContainer from 'bundle-loader?lazy!../modules/patients';
+import Bundle from '../modules/bundle/Bundle';
 
 const Home = ({ isAuthenticated }) => (
 	<div>
 		<h2>Home</h2>
 		<p> Im home!!!</p>
-		<PatientContainer isAuthenticated={isAuthenticated} />
+		<Bundle load={PatientContainer}>
+			{ PatientContainer => (PatientContainer
+			? <PatientContainer isAuthenticated={isAuthenticated} />
+			: <h1>Loading...</h1>
+		)}
+		</Bundle>
+
 	</div>
 );
 
