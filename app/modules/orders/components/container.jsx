4bff2fa37f50bcf667d71d6db111d15795ca39d3
schew <schew@schew.lan>
2017-07-06 10:21:51 -0400

    Added support for multiple orders based on list of orders from api

--- app/modules/orders/components/container.jsx
+++ app/modules/orders/components/container.jsx
@@ -1,25 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Order from './order.jsx';
 
 class Orders extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			title: 'Orders'
+			title: 'Orders',
+			data: []
 		};
 	}
 
+	componentDidMount() {
+		console.log('component mounted');
+		this.getData();
+	}
+
+	getData() {
+		axios.get('https://private-2ab82d-f4orders.apiary-mock.com/orders/?following=false&assigned=false&patientId=pat123&customerId=null')
+			.then((response) => {
+				console.log("Order retrieval successful");
+				this.setState((prevState) => {
+					console.log(response.data.data.orders)
+					return {
+						data: prevState.data.concat(response.data.data.orders)
+					};
+				});
+			}, (error) => {
+				console.log(error);
+			});
+	}
+
 	render() {
-		return (<div>{this.props.title}
-			<Order vendor="meow" name="mers" submission="12-48-1999" />
-			<Order vendor="aim" name="yee" submission="boi" />
-		</div>);
+		return (
+			<div>
+				{this.props.title}
+				{this.state.data.map(function(result, index) {
+					return <Order key={index} vendor={result.id} name={result.deviceId} submission={result.createdAt}/>;
+				})}
+			</div>);
 	}
 }
 
 Orders.propTypes = {
-	title: PropTypes.string.isRequired
+	title: PropTypes.string.isRequired,
+	data: PropTypes.object
 };
 
 export default Orders;
